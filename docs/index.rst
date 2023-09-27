604c1b5551205da0ba51dfa12ddfa256dba404d5
Felix Schilling <felix.s.schilling@gmail.com>
2023-09-27 14:01:02 +0200

    so

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. mining_cadastres_event_study documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+mining_cadastres_event_study documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='How does the administrative capacity building in the mining sector affect its contribution to fiscal capacity? This repo examines with event study how mining cadastre reforms in different countries relate to the liklihood that countries publish their resource revenues and taxes. The underlying data origns from International Aid Transparency Initiative (IATI) database and the UNU-Wider Government Revenue Data set.',
+    author='Felix Schilling',
+    license='',
+)
